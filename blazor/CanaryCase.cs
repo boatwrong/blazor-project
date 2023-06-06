fcbff90c980c6621fa2e34b84006fafb56307b6f
boatwrong <bryantboatright@icloud.com>
2023-06-06 13:13:54 -0500

    update

---
+++ blazor/CanaryCase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace blazor
+{
+	public class CanaryCase
+	{
+		public string? CaseName { get; set; }
+        public bool IsBLEVE { get; set; } = false;
+        public CanaryCase()
+		{
+		}
+	}
+}
+
